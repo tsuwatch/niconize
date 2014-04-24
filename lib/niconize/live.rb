7a2543f2c03a3aef2748ed0074bfca6f398f5f31
Tomohiro Suwa（tsuwatch） <neoen.gsn@gmail.com>
2014-04-24 19:13:40 +0900

    Fix reserved?

--- lib/niconize/live.rb
+++ lib/niconize/live.rb
@@ -13,12 +13,32 @@ class Niconize
       @lv = lv
     end
 
-    def live_page
-      @live_page ||= @agent.get("http://live.nicovideo.jp/watch/#{lv}")
+    def reserve
+      raise TimeshiftError, 'This nicolive is already reserved' if reserved?
+      lv_num = lv.sub(/^lv/, '')
+
+      data = {
+        'mode' => 'regist',
+        'vid' => lv_num,
+        'token' => ulck,
+        '_' => ''
+      }
+
+      @agent.post(URL[:reserve], data)
     end
 
+    private
+
     def reserved?
-      !!live_page.search('a.watching_reservation_reserved')[0]
+      lv_num = lv.sub(/^lv/, '')
+      query = { 'mode' => 'list' }
+      response = @agent.get(URL[:reserve], query)
+      vid_list = response.search('timeshift_reserved_list').children.map { |vid| vid.inner_text }
+      !!vid_list.include?(lv_num)
+    end
+
+    def live_page
+      @live_page ||= @agent.get("http://live.nicovideo.jp/watch/#{lv}")
     end
 
     def token
@@ -36,20 +56,6 @@ class Niconize
       response = @agent.get(URL[:reserve], query)
       raise TimeshiftError, 'It is the limit of the number of your reservation' unless response.at('div.reserve')
       response.at('div.reserve').inner_html.scan(/ulck_[0-9]+/)[0]
-    end
-
-    def reserve
-      raise TimeshiftError, 'This nicolive is already reserved'  unless reserved?
-      lv_num = lv.sub(/^lv/, '')
-
-      data = {
-        'mode' => 'regist',
-        'vid' => lv_num,
-        'token' => ulck,
-        '_' => ''
-      }
-
-      @agent.post(URL[:reserve], data)
     end
 
     class TimeshiftError < StandardError; end
